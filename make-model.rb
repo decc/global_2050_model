28e4e63d572a104db53f6bc678fa2e6f27913d17
Tom Counsell <tom@counsell.org>
2013-12-19 00:26:04 +0000

    Update to version 1.21.0 of the calculator, including a Outputs - misc sheet

--- make-model.rb
+++ make-model.rb
@@ -9,7 +9,7 @@ command.output_directory = 'ext'
 command.output_name = 'global_2050_model'
 
 # Superceeded by named_references_that_can_be_set_at_runtime below
-# command.cells_that_can_be_set_at_runtime = { "User inputs" => (7.upto(46).to_a.map { |r| "E#{r}" }) }
+command.cells_that_can_be_set_at_runtime = { "User inputs" => (7.upto(46).to_a.map { |r| "E#{r}" }) }
 
 command.cells_to_keep = {
   "User inputs" => :all,
@@ -20,45 +20,46 @@ command.cells_to_keep = {
   "Outputs - Land use, technology" => :all,
   "Outputs - Costs" => :all,
   "Outputs - Energy flows" => :all,
+  "Outputs - Misc" => :all
 }
 
-command.named_references_to_keep = [
-  'webtool.1990.to.2050.demand',
-  'webtool.1990.to.2050.supply',
-  'webtool.bio.2011',
-  'webtool.bio.2050',
-  'webtool.buildings.2011.to.2050',
-  'webtool.buildings.historic',
-  'webtool.C02e.timeseries',
-  'Webtool.CO2.50.percent.2C',
-  'webtool.CO2e.total.emissions',
-  'webtool.cumulative.CO2e.2011',
-  'webtool.cumulative.CO2e.2100',
-  'webtool.demand.2011.actual',
-  'webtool.demand.2050',
-  'webtool.diet.2050',
-  'webtool.electricity',
-  'webtool.electricity.2011',
-  'webtool.electricity.2050',
-  'webtool.land.use',
-  'webtool.remaining.coal',
-  'webtool.remaining.gas',
-  'webtool.remaining.oil',
-  'webtool.supply.2011.actual',
-  'webtool.supply.2050',
-  'webtool.transport.2011.to.2050',
-  'webtool.transport.historic',
-  'webtool.vehicles.2011',
-  'webtool.vehicles.2050',
-  'webtool.warming.2100',
-  'webtool.user.choices.description',
-  'webtool.user.choices.limit',
-  'webtool.user.choices.names'
-]
-
-command.named_references_that_can_be_set_at_runtime = [
-  'webtool.user.choices',
-]
+#command.named_references_to_keep = [
+#  'webtool.1990.to.2050.demand',
+#  'webtool.1990.to.2050.supply',
+#  'webtool.bio.2011',
+#  'webtool.bio.2050',
+#  'webtool.buildings.2011.to.2050',
+#  'webtool.buildings.historic',
+#  'webtool.C02e.timeseries',
+#  'Webtool.CO2.50.percent.2C',
+#  'webtool.CO2e.total.emissions',
+#  'webtool.cumulative.CO2e.2011',
+#  'webtool.cumulative.CO2e.2100',
+#  'webtool.demand.2011.actual',
+#  'webtool.demand.2050',
+#  'webtool.diet.2050',
+#  'webtool.electricity',
+#  'webtool.electricity.2011',
+#  'webtool.electricity.2050',
+#  'webtool.land.use',
+#  'webtool.remaining.coal',
+#  'webtool.remaining.gas',
+#  'webtool.remaining.oil',
+#  'webtool.supply.2011.actual',
+#  'webtool.supply.2050',
+#  'webtool.transport.2011.to.2050',
+#  'webtool.transport.historic',
+#  'webtool.vehicles.2011',
+#  'webtool.vehicles.2050',
+#  'webtool.warming.2100',
+#  'webtool.user.choices.description',
+#  'webtool.user.choices.limit',
+#  'webtool.user.choices.names'
+#]
+#
+#command.named_references_that_can_be_set_at_runtime = [
+#  'webtool.user.choices',
+#]
 
 command.actually_compile_code = true
 command.actually_run_tests = true
